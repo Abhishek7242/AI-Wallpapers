29692201125eb09afcdb3aad1ce46f0b8f492c2d
Abhishek Verma <131652685+Abhishek7242@users.noreply.github.com>
2023-06-13 11:59:08 +0530

    new
    optmized js

---
+++ ironman.js
@@ -0,0 +1,35 @@
+let i =0
+let first =document.getElementById('container')
+
+let images =new Image('ironman.jpeg')
+ images = [];
+
+for (let j = 0; j <= 4; j++) {
+  images.push({ filepath: `ironman${j}.jpg` });
+}
+
+for(i;i<images.length;i++){
+    
+    let div = document.createElement('div')
+    div.classList.add('divbox')
+    let img = document.createElement('img')
+    img.id='first'+i
+    
+    img.style.maxWidth='400px'
+    img.style.maxHeight='600px'
+    img.src=images[i].filepath
+    let btn = document.createElement('button')
+    btn.id='btn'+i
+    btn.classList.add('btncss')
+    btn.innerText='Download'
+    
+    btn.addEventListener('click', function() {
+        let a = document.createElement('a');
+        a.href = img.src;
+        a.download = 'image';
+        a.click();
+    });
+    first.appendChild(div)
+    div.appendChild(img)
+    div.appendChild(btn)
+}
